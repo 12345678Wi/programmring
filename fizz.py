e4dc7c813f8d87bd8d824e12944da553dbf789a5
12345678Wi <william.hedinulander@gmail.com>
2025-09-25 15:18:10 +0200

    1234

---
+++ fizz.py
@@ -0,0 +1,15 @@
+print ("pick a nummber you wannt to start on")
+ 
+number1 = input()
+number1 = int(number1)
+
+
+print ("pick a nummber you wanna to end on")
+
+number2 = (input)
+number2 = int (number2)
+
+for i in range (number1, number2):
+    print (i)
+
+    if i == 3,6,
---
+++ i.py
@@ -0,0 +1,20 @@
+
+ word = "hej på dig!"
+
+
+ for lettel in word:
+   print(f"''(lettel)'!")
+
+
+   sentence = "hej på dig , vad gör du?"
+alphabet = "abcdefghjiklmnopqrtsuvwyxzåäö"
+
+
+
+
+
+
+
+
+
+print ("fisrt nummber")
